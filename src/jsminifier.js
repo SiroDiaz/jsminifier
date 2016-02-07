cbe698f1c4d75dcb1b020f303fbb973572ccd242
Siro_Diaz <siro_diaz@yahoo.com>
2016-02-07 16:38:21 +0100

    Single js file minify and uglify the dist file

--- src/jsminifier.js
+++ src/jsminifier.js
@@ -1,8 +1,28 @@
-import {readFile} from "fs"
+import {readFileSync} from "fs"
 
 class JSminifier {
 	constructor(file) {
-		this.file = file;
+		this.file = file
+	}	
+	minify() {
+		let data = readFileSync(this.file, 'utf8');
+		data = data.replace(/\r|\t/g, "")
+		let lines = data.split('\n')
+		
+		for(let i = 0; i < lines.length; i++) {
+			lines[i] = lines[i].trim()
+
+			if(!/(\(|\{)$/.test(lines[i]) && lines[i].length > 0) {
+				if(!/\;$/g.test(lines[i]) && !/(\}|\,|\+|\:)$/.test(lines[i])) {
+					if(!/^((\'|\")(.*?)(\'|\"))$/.test(lines[i])) {
+						lines[i] += ';'
+					}
+				}
+			}
+		}
+
+		return lines.join('\n').replace(/\n/g, '')
 	}
-	
 }
+
+module.exports = JSminifier
